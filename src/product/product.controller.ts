69169a5b5fe6b30fded909280dbd7ba0d107431c
santhosh-natarajan <santhosh.swengineer@gmail.com>
2023-08-30 10:32:09 +0530

    Product mrp logic updated

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -14,6 +14,41 @@ export class ProductController {
     @UsePipes(new ValidationPipe())
     createProduct(@Body() newProductData: ProductDto) { 
         console.log("New product data", newProductData);
+        const productData = newProductData;
+        productData.sgst = this.calculateSGST(productData.type, productData.price);
+        productData.cgst = this.calculateCGST(productData.type, productData.price);
+        productData.mrp = this.calculateMRP(productData.sgst, productData.cgst, productData.price);
+        console.log("updated product data", productData); 
+    }
+
+    private calculateSGST = (productType:string, productPrice: number) => { 
+        let  sgstAmount = 0;
+        if (productType === 'A') {
+            return sgstAmount = ((productPrice) * 2.5) / 100;
+        } else if (productType === 'B') {
+            return sgstAmount = ((productPrice) * 2.5) / 100;
+        } else if (productType === 'C') {
+            return sgstAmount = ((productPrice) * 2.5) / 100;
+        } else {
+            return sgstAmount;
+        }
+    }
+
+    private calculateCGST = (productType:string, productPrice:number) => { 
+        let  cgstAmount = 0;
+        if (productType === 'A') {
+            return cgstAmount = ((productPrice) * 2.5) / 100;
+        } else if (productType === 'B') {
+            return cgstAmount = ((productPrice) * 2.5) / 100;
+        } else if (productType === 'C') {
+            return cgstAmount = ((productPrice) * 2.5) / 100;
+        } else {
+            return cgstAmount;
+        }
+    }
+
+    private calculateMRP = (sgst:number, cgst:number, price:number) => { 
+        return sgst+cgst+price;
     }
 
 }
